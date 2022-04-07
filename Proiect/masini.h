291be779c39f90a97bd6d20c2617f0ca172bf3f2
Andrei Cerbulescu <andrei.cerbulescu@gmail.com>
2022-04-07 19:41:25 +0300

    ✨adaugat faruri la masinile care vin inainte sa vina politia

--- Proiect/masini.h
+++ Proiect/masini.h
@@ -2,7 +2,7 @@
 #include <GL/freeglut.h>
 
 extern double loc_vert, height;
-extern int este_politie;
+extern int este_politie, urmeaza_politie;
 
 void deseneaza_masina() {
 	glPushMatrix();
@@ -14,7 +14,29 @@ void deseneaza_masina() {
 	if (este_politie) {
 		glColor3f(1, 0.0, 0.0);
 		glRecti(-30, -10, -15, 0);
-	}	
+	}
+
+	if (urmeaza_politie) {
+		glBegin(GL_TRIANGLES);
+
+		glColor3f(1.0, 1.0, 0);
+		glOrtho(-30.0, -45.0, -45.0, -2.5, 1.5, 5.5);
+		glVertex2f(-45.0, -5.0);
+		glVertex2f(-60.0, 10.0);
+		glVertex2f(-60.0, -20.0);
+
+		glEnd();
+
+		glBegin(GL_TRIANGLES);
+
+		glColor3f(1.0, 1.0, 0);
+
+		glVertex2f(-45.0, 5.0);
+		glVertex2f(-60.0, 20.0);
+		glVertex2f(-60.0, -10.0);
+
+		glEnd();
+	}
 
 	glPopMatrix();
 }
