ac3b7e9b8c0d1c9fbae180183ac374f1dd9a4312
Andrei Cerbulescu <andrei.cerbulescu@gmail.com>
2022-04-17 15:31:58 +0300

    commit prezentare

--- Proiect/bara_progres.h
+++ Proiect/bara_progres.h
@@ -4,10 +4,16 @@
 extern double timp, ok;
 extern double progres;
 double total_drum = 12000;
+extern float score, temp_mancare;
 
 void deseneaza_bara_progres() {
 	if (ok) {
 		progres += timp;
+	}
+	if (progres / total_drum > 1) {
+		progres = 0;
+		score += temp_mancare;
+		temp_mancare = 100;
 	}
 	glPushMatrix();
 	glTranslated(250.0, -100.0, 0.0);
